b63c45f03b01aa693f639484c124867df997b08f
122L <og153182@students.davenport.k12.ia.us>
2020-03-12 13:42:58 -0500

    bruh

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,17 +1,20 @@
 package Hangman;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Hangman {
     public static void main(String[] args) {
-        String[] words = {"Bruh", "Guacamole", "Jouch", "Fortnite", "Beans", ""};
+        String[] words = {"Bruh", "Guacamole", "Jouch", "Fortnite", "Beans", "Smoothbrain", "Chair"};
         Scanner input = new Scanner(System.in);
         int randomInt;
-        randomInt = ((int)(Math.random() * 100) % 7);
+        randomInt = ((int)(Math.random() * words.length));
         String randomWord = words[randomInt];
-        System.out.println(randomWord);
-        Path WhoWon = 
+        Path WhoWon = Paths.get("C:\\Users\\og153182\\Desktop\\Java\\Hangman\\src\\Hangman\\WhoWon.txt");
 
+        for(int i = 0; i < randomWord.length(); ++i) {
+            System.out.println();
+        }
     }
 }
